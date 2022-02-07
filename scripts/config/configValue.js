8272936863605bdc4ef9bdac7b2851d8af14b61c
Gian Dal Pont <gian.pont@hneducacional.com.br>
2022-02-07 02:39:53 -0300

    Ajuste https

--- scripts/config/configValue.js
+++ scripts/config/configValue.js
@@ -1,5 +1,5 @@
 angular.module("marvel").constant("config", {
-  baseUrl: "http://gateway.marvel.com/v1/public/",
+  baseUrl: "https://gateway.marvel.com/v1/public/",
   // publicKey: "5a237863b3cc2061003cbbc4fe20dc06",
   // // apikey: "468a513f0aa44ea7eba5397858f20b5b",
   // privateKey: "fbf255068eccea6d0ef951b9f25626b57ab2fe72",
