e329d11d09a8c38a4bbfe36b2dfd5874b76de436
Gian Dal Pont <gian.pont@hneducacional.com.br>
2022-02-07 02:33:03 -0300

    Ajuste configuração API

--- scripts/config/configValue.js
+++ scripts/config/configValue.js
@@ -1,5 +1,8 @@
 angular.module("marvel").constant("config", {
-  baseUrl: "http://gateway.marvel.com:80/v1/public/",
-  apikey: "5a237863b3cc2061003cbbc4fe20dc06",
-  hash: "fbf255068eccea6d0ef951b9f25626b57ab2fe72",
+  baseUrl: "http://gateway.marvel.com/v1/public/",
+  // publicKey: "5a237863b3cc2061003cbbc4fe20dc06",
+  // // apikey: "468a513f0aa44ea7eba5397858f20b5b",
+  // privateKey: "fbf255068eccea6d0ef951b9f25626b57ab2fe72",
+  publicKey: "d9c7bfd558e42ea50756c5a8ea50d9af",
+  privateKey: "bedd4a83b71ab04e5889f7be44f0522e278d8056",
 });
--- scripts/services/marvel.js
+++ scripts/services/marvel.js
@@ -1,17 +1,24 @@
 app.factory('marvel', ['$http', 'config', function($http, config) {
 
+  var timestamp = Date.now();
+  var hash = md5(timestamp + config.privateKey + config.publicKey);
+
+
   var _search = function (search = null) {
     var url = config.baseUrl + "characters";
+    
 
     if (!search) {
       return $http
-        .get(config.baseUrl + "characters?apikey=" + config.apikey, {
-          params: {
-            ts: new Date(),
-            apikey: config.apikey,
-            hash: config.hash,
-          },
-        })
+        .get(
+          config.baseUrl +
+            "characters?ts=" +
+            timestamp +
+            "&apikey=" +
+            config.publicKey +
+            "&hash=" +
+            hash,
+        )
         .success(function (data) {
           return data;
         })
@@ -25,9 +32,9 @@ app.factory('marvel', ['$http', 'config', function($http, config) {
         params: {
           nameStartsWith: search,
           limit: 24,
-          ts: new Date(),
-          apikey: config.apikey,
-          hash: config.hash,
+          ts: timestamp,
+          apikey: config.publicKey,
+          hash: hash,
         },
       })
       .success(function (data) {
@@ -39,7 +46,7 @@ app.factory('marvel', ['$http', 'config', function($http, config) {
   };
 
   var _getId = function (id) {
-    var url = config.baseUrl + "characters/"+id+"?apikey="+config.apikey;
+    var url = config.baseUrl + "characters/" + id + "?ts=" + timestamp + "&apikey=" + config.publicKey + "&hash=" + hash;
 
     return $http
       .get(url)
